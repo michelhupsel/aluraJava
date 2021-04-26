244f7a330863895fad03bec5e0aea69c211d4bdd
80722728 <michel.silva@capgemini.com>
2021-04-26 09:06:47 -0300

    06.Finally e try with resources

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -1,79 +1,77 @@
 public abstract class Conta {
 
-    protected double saldo;
-    private int agencia;
-    private int numero;
-    private Cliente titular;
-    private static int total = 0;
+	protected double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
 
-    
-    public Conta(int agencia, int numero){
-        Conta.total++;
-        //System.out.println("O total de contas é " + Conta.total);
-        this.agencia = agencia;
-        this.numero = numero;
-        //this.saldo = 100;
-        //System.out.println("Estou criando uma conta " + this.numero);
-    }
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		// System.out.println("O total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		// this.saldo = 100;
+		// System.out.println("Estou criando uma conta " + this.numero);
+	}
 
-    public abstract void deposita(double valor);
+	public abstract void deposita(double valor);
 
-    public boolean saca(double valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public void saca(double valor) {
 
-    public boolean transfere(double valor, Conta destino) {
-        if(this.saca(valor)) {
-                destino.deposita(valor);
-                return true;
-        } else {
-                return false;
-        }
-    }
+		if (this.saldo < valor) {
+			// usao da exception
+			throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
+		}
 
-    public double getSaldo(){
-        return this.saldo;
-    }
+		this.saldo -= valor;
 
-    public int getNumero(){
-        return this.numero;
-    }
+	}
 
-    public void setNumero(int numero){
-        if(numero <= 0) {
-            System.out.println("Nao pode valor menor igual a 0");
-            return;
-        }
-        this.numero = numero;
-    }
+	public void transfere(double valor, Conta destino) {
+		this.saca(valor);
+		destino.deposita(valor);
 
-    public int getAgencia(){
-        return this.agencia;
-    }
+	}
 
-    public void setAgencia(int agencia){
-       if(agencia <= 0) {
-           System.out.println("Nao pode valor menor igual a 0");
-           return;
-       }
-       this.agencia = agencia;
-    }
+	public double getSaldo() {
+		return this.saldo;
+	}
 
-    public void setTitular(Cliente titular){
-        this.titular = titular;
-    }
+	public int getNumero() {
+		return this.numero;
+	}
 
-    public Cliente getTitular(){
-        return this.titular;
-    }
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.numero = numero;
+	}
 
-    public static int getTotal(){
-        return Conta.total;
-    }
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
 
 }
---
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -0,0 +1,17 @@
+
+public class TesteSaca {
+
+	public static void main(String[] args) {
+
+		Conta conta = new ContaCorrente(123, 321);
+		conta.deposita(200.00);
+		try {
+			conta.saca(210.0);
+		} catch (SaldoInsuficienteException e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+		
+		System.out.println(conta.getSaldo());
+	}
+
+}
