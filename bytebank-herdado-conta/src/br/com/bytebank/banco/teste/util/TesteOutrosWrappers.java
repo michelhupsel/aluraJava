7928aba076df7dad678634e5bf62fb4530d9f65c
michelhupsel <michelhupsel@gmail.com>
2021-05-15 00:08:56 -0300

    As classes Wrappers

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteOutrosWrappers.java
@@ -0,0 +1,27 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteOutrosWrappers {
+	public static void main(String[] args) {
+		
+		
+		Integer idadeRef = Integer.valueOf(29); //autoboxing
+		System.out.println(idadeRef.doubleValue()); //unboxing
+		
+		
+		Double dRef = Double.valueOf(3.2);
+		System.out.println(dRef.doubleValue());
+		
+		Boolean bRef = Boolean.FALSE;
+		
+		Number refNumero = Float.valueOf(29.9f);
+		
+		List<Number> lista =  new ArrayList<>();
+		lista.add(10);
+		lista.add(32.6);
+		lista.add(25.6f);
+		
+	}
+}
