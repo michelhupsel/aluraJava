e274fa7c3557d2bf7901a62c0998912f579696e9
michelhupsel <michelhupsel@gmail.com>
2021-10-11 06:54:52 -0300

    xml com java

---
+++ xml-com-java/src/br/com/alura/Model/Produto.java
@@ -0,0 +1,26 @@
+package br.com.alura.Model;
+
+public class Produto {
+
+	private String nome;
+	private double preco;
+
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + "\nPreço: " + preco + "\n";
+	}
+
+}
