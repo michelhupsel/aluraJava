8d57deb7083f769e2851c36fa551309bf9fe7e9e
michelhupsel <michelhupsel@gmail.com>
2021-10-19 23:58:26 -0300

    testes com XPath

---
+++ xml-com-java/src/br/com/alura/Teste/LeXmlComXPathArquivosDiretorio.java
@@ -0,0 +1,72 @@
+package br.com.alura.Teste;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import br.com.alura.Model.Produto;
+
+public class LeXmlComXPathArquivosDiretorio {
+	public static void main(String[] args) throws Exception {
+
+		List<Produto> produtos = new ArrayList<Produto>();
+
+		// pegar a quantidade de arquivos dentro do diretório
+		int contador = 0;
+		File pasta = new File("src/xml-files");
+		File[] listaArquivos = pasta.listFiles();
+		// System.out.println(listaArquivos.length);
+
+
+		// iterar cada arquivo xml
+		for (File file : listaArquivos) {
+
+			formataPath(file.toString()); //pode ser chamada direto no builder.parse
+
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.parse(file);
+
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			String expression = "/venda/produtos/produto";
+
+			XPathExpression xPathExpression = xPath.compile(expression);
+			NodeList lista = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
+
+			for (int i = 0; i < lista.getLength(); i++) {
+				Element produto = (Element) lista.item(i);
+
+				String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+				Double preco = Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent());
+
+				Produto prod = new Produto();
+				prod.setNome(nome);
+				prod.setPreco(preco);
+
+				produtos.add(prod);
+			}
+
+		}
+		System.out.println(produtos);
+		
+	}
+
+	
+	//criado para alterar as barras do caminho dos arquivos
+	private static String formataPath(String file) {
+		file = file.replaceAll("\\\\", "/");
+		return file;
+	}
+}
--- xml-com-java/src/br/com/alura/Teste/LeXmlStax.java
+++ xml-com-java/src/br/com/alura/Teste/LeXmlStax.java
@@ -7,26 +7,42 @@ import java.util.List;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 
 import br.com.alura.Model.Produto;
 
-public class LeArquivoXmlTerceiraForma {
+public class LeXmlStax {
 
 	public static void main(String[] args) throws Exception {
 		InputStream is = new FileInputStream("src/vendas.xml");
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		XMLEventReader eventos = factory.createXMLEventReader(is);
 
-		Produto produto = new Produto();
+		// Produto produto = new Produto();
 		List<Produto> produtos = new ArrayList<>();
-		while (eventos.hasNext()) {
 
+		while (eventos.hasNext()) {
+			// pega o proximo evento
 			XMLEvent evento = eventos.nextEvent();
 
 			if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("produto")) {
-				produto = new Produto();
-			} else if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("nome")) {
+				Produto produto = criaUmProduto(eventos);
+				produtos.add(produto);
+			}
+
+		}
+
+		System.out.println(produtos);
+	}
+
+	private static Produto criaUmProduto(XMLEventReader eventos) throws Exception {
+		Produto produto = new Produto();
+
+		while (eventos.hasNext()) {
+			XMLEvent evento = eventos.nextEvent();
+
+			if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("nome")) {
 				evento = eventos.nextEvent();
 				String nome = evento.asCharacters().getData();
 				produto.setNome(nome);
@@ -36,12 +52,10 @@ public class LeArquivoXmlTerceiraForma {
 				double preco = Double.parseDouble(conteudo);
 				produto.setPreco(preco);
 			} else if (evento.isEndElement() && evento.asEndElement().getName().getLocalPart().equals("produto")) {
-				produtos.add(produto);
+				break;
 			}
-
 		}
-		
-		System.out.println(produtos);
+		return produto;
 	}
 
 }
