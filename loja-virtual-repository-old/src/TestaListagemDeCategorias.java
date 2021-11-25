11a9f2a16ff945fa069cc2d72f5357605da5bd08
michelhupsel <michelhupsel@gmail.com>
2021-11-24 23:07:12 -0300

    app desk

--- loja-virtual-repository-old/src/TestaListagemDeCategorias.java
+++ loja-virtual-repository-old/src/TestaListagemDeCategorias.java
@@ -5,19 +5,26 @@ import java.util.List;
 import br.com.alura.jdbc.ConnectionFactory;
 import br.com.alura.jdbc.dao.CategoriaDAO;
 import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
 
 public class TestaListagemDeCategorias {
 
 	public static void main(String[] args) throws SQLException {
-		
-		
-		try(Connection connection = new ConnectionFactory().recuperarConexao()){
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
-			List<Categoria> listaDeCategorias = categoriaDAO.listar();
-			
-			listaDeCategorias.stream().forEach(ct -> System.out.println(ct.getNome()));
+			List<Categoria> listaDeCategorias = categoriaDAO.listarComProdutos();
+
+			listaDeCategorias.stream().forEach(ct -> {
+
+				System.out.println(ct.getNome());
+				for (Produto produto : ct.getProdutos()) {
+					System.out.println(ct.getNome() + " - " + produto.getNome());
+				}
+
+			});
 		}
-		
+
 	}
 
 }
--- loja-virtual-repository-old/src/br/com/alura/jdbc/dao/CategoriaDAO.java
+++ loja-virtual-repository-old/src/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
 
 public class CategoriaDAO {
 
@@ -20,17 +21,46 @@ public class CategoriaDAO {
 	public List<Categoria> listar() throws SQLException {
 		List<Categoria> categorias = new ArrayList<>();
 
+		System.out.println("Executando a query de listar categoria");
+
 		String sql = "SELECT ID, NOME FROM CATEGORIA";
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getResultSet()) {
+				while (rst.next()) {
+					Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+
+					categorias.add(categoria);
+				}
+			}
+		}
+		return categorias;
+	}
+
+	public List<Categoria> listarComProdutos() throws SQLException{
+		Categoria ultima = null;
+		List<Categoria> categorias = new ArrayList<>();
+
+		System.out.println("Executando a query de listar categoria");
+		
+		String sql = "SELECT C.ID, C.NOME, P.NOME, P.DESCRICAO FROM CATEGORIA C INNER JOIN "
+				+ "PRODUTO P ON C.ID = P.CATEGORIA_ID";
 
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 			pstm.execute();
 			
 			try(ResultSet rst = pstm.getResultSet()){
 				while(rst.next()) {
-					Categoria categoria = 
-							new Categoria(rst.getInt(1), rst.getString(2));
-					
-					categorias.add(categoria);
+					if(ultima == null || !ultima.getNome().equals(rst.getString(2))) {
+						Categoria categoria = 
+								new Categoria(rst.getInt(1), rst.getString(2));
+						ultima = categoria;
+						categorias.add(categoria);
+					}
+					Produto produto = new Produto(rst.getString(3), rst.getString(4));
+					ultima.adicionar(produto);
 				}
 			}
 		}
